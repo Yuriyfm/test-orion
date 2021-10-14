23e643eea48e07c7554c26fd00845a63dcc36d30
Yuriy Efimov <79786411+Yuriyfm@users.noreply.github.com>
2021-10-15 00:25:16 +0700

    Update data_validation.py

--- orion/data_validation.py
+++ orion/data_validation.py
@@ -165,21 +165,21 @@ def sort_list_data_validation(request_data, func_name):
         if func_name == 'get_persons_list' and request_data['sorted_by'] not in attributes_for_sort_persons_list:
             return f'Неверный формат значения атрибута sorted_by. Ожидается один из следующих вариантов -' \
                    f' {attributes_for_sort_persons_list}, было получено -  {request_data["sorted_by"]}'
-        if func_name == 'get_persons_list' and request_data['"order'] not in order:
+        if func_name == 'get_persons_list' and request_data["order"] not in order:
             return f'Неверный формат значения атрибута order. Ожидается один из следующих вариантов -' \
-                   f' {order}, было получено -  {request_data["sorted_by"]}'
+                   f' {order}, было получено -  {request_data["order"]}'
         elif func_name == 'get_phones_list' and request_data['sorted_by'] not in attributes_for_sort_phones_list:
             return f'Неверный формат значения атрибута sorted_by. Ожидается один из следующих вариантов -' \
                    f' {attributes_for_sort_phones_list}, было получено -  {request_data["sorted_by"]}'
-        if func_name == 'get_phones_list' and request_data['"order'] not in order:
+        if func_name == 'get_phones_list' and request_data["order"] not in order:
             return f'Неверный формат значения атрибута order. Ожидается один из следующих вариантов -' \
-                   f' {order}, было получено -  {request_data["sorted_by"]}'
+                   f' {order}, было получено -  {request_data["order"]}'
         elif func_name == 'get_emails_list' and request_data['sorted_by'] not in attributes_for_sort_emails_list:
             return f'Неверный формат значения атрибута sorted_by. Ожидается один из следующих вариантов -' \
                    f' {attributes_for_sort_emails_list}, было получено -  {request_data["sorted_by"]}'
-        if func_name == 'get_phones_list' and request_data['"order'] not in order:
+        if func_name == 'get_phones_list' and request_data["order"] not in order:
             return f'Неверный формат значения атрибута order. Ожидается один из следующих вариантов -' \
-                   f' {order}, было получено -  {request_data["sorted_by"]}'
+                   f' {order}, было получено -  {request_data["order"]}'
     except TypeError:
         return ('Нет доступных данных для обработки или формат данных не соответсвует ожидаемому')
     except KeyError as k:
